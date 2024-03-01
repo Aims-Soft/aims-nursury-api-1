7d7f179b643dcab67af4664d555ecd1b09a5dc7e
hammadhere12 <hammadnoor120@gmail.com>
2024-03-01 10:45:17 +0500

    new controller "OpeningController"

---
+++ posCoreModuleApi/Controllers/OpeningController.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using posCoreModuleApi.Services;
+using Microsoft.Extensions.Options;
+using posCoreModuleApi.Configuration;
+using posCoreModuleApi.Entities;
+using Dapper;
+using System.Data;
+using Npgsql;
+using System.Collections.Generic;
+
+namespace posCoreModuleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OpeningController : ControllerBase
+    {
+        private readonly IOptions<conStr> _dbCon;
+        private readonly dapperQuery _dapperQuery;
+        private string cmd, cmd2;
+        public string saveConStr;
+
+        public OpeningController(dapperQuery dapperQuery,IOptions<conStr> dbCon)
+        {
+            _dbCon = dbCon;
+            _dapperQuery = dapperQuery;
+        }
+    
+        
+    }
+}
